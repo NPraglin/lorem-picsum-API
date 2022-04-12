739fa1a931cd39d59883acce0853054ef3ecad3f
NPraglin <npraglin@gmail.com>
2022-04-12 09:38:29 -0500

    updatred for dep

--- api_proj/settings.py
+++ api_proj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!6=j^67xpsht)@s_8mv9x6wj**$m_n(5a1=gbx(b86(uh+#j%='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
